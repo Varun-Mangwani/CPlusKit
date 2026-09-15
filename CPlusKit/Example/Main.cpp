2565de23bf134a50979cc16b74ddb151f65abd16
Varun Mangwani <vpm.media.business@gmail.com>
2026-09-15 20:48:21 +0000

    Updates From Varun

--- CPlusKit/Example/Main.cpp
+++ CPlusKit/Example/Main.cpp
@@ -4,6 +4,10 @@ using namespace cpk;
 
 int main() {
     int value = -5;
-    std::cout << "Absolute value of " << value << " is: " << math::abs(value) << std::endl;
+    // std::cout << "Absolute value of " << value << " is: " << math::abs(value) << std::endl;
+    // std::cout << std::endl << cpk::factorial(5) << std::endl;
+    // std::cout << std::endl << cpk::IsPrime(6) << std::endl;
+    // std::cout << std::endl << cpk:: << std::endl;
+    
     return 0;
 }
--- CPlusKit/cpluskit.hpp
+++ CPlusKit/cpluskit.hpp
@@ -76,10 +76,69 @@ namespace cpk
         return (val % 2 == 0);
     }
 
-    //IsOdd
-    int IsOdd(int val){
+    // IsOdd
+    int IsOdd(int val)
+    {
         return (val % 2 != 0);
     }
 
+    // Factorial Function
+    int factorial(int val)
+    {
+        int fact = 1;
+        for (int i = 1; i <= val; i++)
+        {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    // IsPrime Function
+    int IsPrime(int val)
+    {
+
+        int flag = 1;
+
+        for (int i = 2; i < val; i++)
+        {
+            if (val % i == 0)
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+}
+
+//digits function
+
+int* digits(int val) {
+
+    int dgt,temp;
+    temp = val;
+    int count = 0;
+    
+    while (val != 0)
+    {
+        count++;
+    }
+    int arr[count];
+    for (int i = count; i >= 0; i++)
+    {
+        dgt = temp % 10;
+        arr[i] = dgt;
+        temp /= 10;
+        
+    }
+    return arr;
     
 }
