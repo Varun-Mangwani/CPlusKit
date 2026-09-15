19dfddabf8452a459bc71e7af3917d6bdfcbbf5b
Varun Mangwani <vpm.media.business@gmail.com>
2026-09-15 12:07:34 +0000

    updates by varun

--- CPlusKit/cpluskit.hpp
+++ CPlusKit/cpluskit.hpp
@@ -1,27 +1,85 @@
-#include<iostream>
+#include <iostream>
 
+// abs
+// min
+// max
+// clamp
+// sign
+
+// isEven
+// isOdd
+
+// gcd
+// lcm
+// factorial
+// isPrime
+
+// digits
+// sumDigits
+// productDigits
+// reverseNumber
+
+// isPalindrome
+// isArmstrong
+// isPerfectNumber
+// isStrongNumber
+
+// countDivisors
+// sumDivisors
+// getDivisors
+// getPrimeFactors
+
+// permutation
+// combination
 using namespace std;
 
-namespace cpk {
-    namespace math{
-        //absolute value function
-        int abs(int val) {
-            if(val < 0) {
+namespace cpk
+{
+    namespace math
+    {
+        // absolute value function
+        int abs(int val)
+        {
+            if (val < 0)
+            {
                 return -val;
             }
             return val;
         }
     }
 
-    //Minimum Function
+    // Minimum Function
     int min(int a, int b)
     {
         return (a > b) ? b : a;
     }
 
-    //Maximum Function
+    // Maximum Function
     int max(int a, int b)
     {
         return (a > b) ? a : b;
     }
+
+    // clamp function
+    int clamp(int val, int minVal, int maxVal)
+    {
+        if (val < minVal)
+            return minVal;
+        if (val > maxVal)
+            return maxVal;
+        return val;
+    }
+
+    // IsEven
+    int IsEven(int val)
+    {
+        return (val % 2 == 0);
+    }
+
+    //IsOdd
+    int IsOdd(int val){
+        return (val % 2 != 0);
+    }
+
+    
 }
