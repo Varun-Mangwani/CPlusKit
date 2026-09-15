64ddf00d904680eef006d56724d1a7a23df63fe1
Varun Mangwani <vpm.media.business@gmail.com>
2026-09-15 20:49:30 +0000

    Updates

--- CPlusKit/Example/Main.cpp
+++ CPlusKit/Example/Main.cpp
@@ -3,7 +3,7 @@
 using namespace cpk;
 
 int main() {
-    int value = -5;
+    // int value = -5;
     // std::cout << "Absolute value of " << value << " is: " << math::abs(value) << std::endl;
     // std::cout << std::endl << cpk::factorial(5) << std::endl;
     // std::cout << std::endl << cpk::IsPrime(6) << std::endl;
